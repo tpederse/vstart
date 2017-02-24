193495461b47ed75337c3637a16ab0abab4bbdda
Trond Pedersen <badboypedersen@gmail.com>
2017-02-24 20:13:58 +0100

    simple login webclient

--- server/src/main/webapp/commonLocal.js
+++ server/src/main/webapp/commonLocal.js
@@ -1,1 +1,25 @@
-$("#menu").load("/menu.html");
+var simpleLogin= function(){
+    var dataToSend={
+        username: $("#username").val(),
+    }
+
+    $.ajax({
+        method: "POST",
+        url: "/api/login",
+        data: JSON.stringify(dataToSend),
+        success: function( fromServer ) {
+            console.log(fromServer);
+
+        },
+        error: function( jqXHR, textStatus, errorThrown ){
+            console.log(jqXHR);
+        }
+    });
+}
+
+var enableMenu= function(){
+    $("#loginButton").click(simpleLogin);
+
+}
+
+$("#menu").load("/menu.html",enableMenu);
